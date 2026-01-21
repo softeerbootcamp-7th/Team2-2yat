4bf6974e6b2b5e7e0771fd6df5480f7aaa1a6835
arty0928 <arty0928@konkuk.ac.kr>
2026-01-21 11:46:21 +0900

    chore: eslint.config 파일에 plugin, rule 추가 #72

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,23 @@
 import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-        plugins: { js },
+        plugins: {
+            js,
+            "simple-import-sort": simpleImportSort,
+        },
         extends: ["js/recommended"],
         languageOptions: { globals: globals.browser },
         rules: {
             "react/react-in-jsx-scope": false,
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
         },
     },
     tseslint.configs.recommended,
