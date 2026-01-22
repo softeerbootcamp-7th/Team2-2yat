3a9f88c3c6abb00ddc5c8f3919c06fde0c339759
arty0928 <arty0928@konkuk.ac.kr>
2026-01-22 18:50:40 +0900

    feat: MenuNodeButton 구현

---
+++ frontend/src/features/mindmap/node/menu_node/MenuNodeButton.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@utils/cn";
+import { getColorClass } from "@features/mindmap/node/utils/colors";
+import type { NodeComponentProps } from "@features/mindmap/node/types/node";
+import Icon from "@shared/components/icon/Icon";
+
+export default function MenuNodeButton({ color, className }: NodeComponentProps) {
+    return (
+        <div
+            className={cn(
+                "w-3 h-3 cursor-pointer rounded-bl-md rounded-tr-md justify-center items-center flex",
+                getColorClass({ color }),
+                className,
+            )}
+        >
+            <Icon name="ic_ellipsis" size={8} color="var(--color-base-white)" />
+        </div>
+    );
+}
