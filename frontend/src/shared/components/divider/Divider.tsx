4a58891220e732e0dc24172a789e7f1cb21a93b6
pakxe <pigkill40@naver.com>
2026-01-25 00:57:41 +0900

    refactor: Divider의 default direction을 "x"로 변경

--- frontend/src/shared/components/divider/Divider.tsx
+++ frontend/src/shared/components/divider/Divider.tsx
@@ -5,10 +5,10 @@ type Props = React.ComponentPropsWithoutRef<"div"> & {
     direction?: Direction;
 };
 
-const Divider = ({ direction = "y", className, ...props }: Props) => {
+const Divider = ({ direction = "x", className, ...props }: Props) => {
     return <div className={cn(`bg-gray-300 ${directionStyles(direction)}`, className)} {...props} />;
 };
 
 export default Divider;
 
-const directionStyles = (direction: Direction) => (direction === "y" ? "w-full h-[1px]" : "h-full w-[1px]");
+const directionStyles = (direction: Direction) => (direction === "x" ? "w-full h-[1px]" : "h-full w-[1px]");
