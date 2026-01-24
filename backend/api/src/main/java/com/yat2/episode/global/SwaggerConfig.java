bf4c968dd40f57a312400ee23b5c0f2a0b272242
kimhji <twin4185@naver.com>
2026-01-24 19:45:56 +0900

    feat: 예시 디스크립션 추가

--- backend/api/src/main/java/com/yat2/episode/global/SwaggerConfig.java
+++ backend/api/src/main/java/com/yat2/episode/global/SwaggerConfig.java
@@ -7,29 +7,34 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("access_token")
+                .description("입력하지 마시고, 로그인 api를 실행해주세요.");
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("refresh_token")
+                .description("입력하지 마시고, 로그인 api를 실행해주세요.");
+
+        Components components = new Components()
+                .addSecuritySchemes("COOKIE_ACCESS_TOKEN", accessTokenScheme)
+                .addSecuritySchemes("COOKIE_REFRESH_TOKEN", refreshTokenScheme);
+
+        SecurityRequirement globalRequirement = new SecurityRequirement()
+                .addList("COOKIE_ACCESS_TOKEN");
+
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList("COOKIE_ACCESS_TOKEN")
-                        .addList("COOKIE_REFRESH_TOKEN")
-                )
-                .components(new Components()
-                        .addSecuritySchemes("COOKIE_ACCESS_TOKEN",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.APIKEY)
-                                        .in(SecurityScheme.In.COOKIE)
-                                        .name("access_token")
-                        )
-                        .addSecuritySchemes("COOKIE_REFRESH_TOKEN",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.APIKEY)
-                                        .in(SecurityScheme.In.COOKIE)
-                                        .name("refresh_token")
-                                )
-                );
+                .components(components)
+                .security(List.of(globalRequirement));
     }
 }
