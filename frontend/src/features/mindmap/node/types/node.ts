7a3a8af8750ed6f9c264219248381afd6b3217af
arty0928 <arty0928@konkuk.ac.kr>
2026-01-25 00:38:52 +0900

    fix: getColorStyle bg외에 범용적으로 적용할 수 있도록 값만 반환  및 opacity 추가

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -3,5 +3,6 @@ import { NODE_COLORS } from "@features/mindmap/node/utils/colors";
 export type NodeComponentProps = {
     colorIndex?: number;
     color?: (typeof NODE_COLORS)[number];
+    opacity?: number;
     className?: string;
 };
--- frontend/src/features/mindmap/node/utils/colors.ts
+++ frontend/src/features/mindmap/node/utils/colors.ts
@@ -2,13 +2,16 @@ import type { NodeComponentProps } from "@features/mindmap/node/types/node";
 
 export const NODE_COLORS = ["violet", "blue", "skyblue", "mint", "cyan", "purple", "magenta", "navy"] as const;
 
-export const getColorClass = ({ colorIndex = 0, color }: NodeComponentProps) => {
-    // color 문자열이 있으면 우선 사용
+/*
+/**
+ * CSS 변수 형태로 색상 스타일 반환
+ */
+export function getColorStyle({ colorIndex, color, opacity }: NodeComponentProps): string {
     if (color) {
-        return `bg-node-${color}-op-100`;
+        console.log(color);
+        return `var(--color-node-${color}-op-${opacity})`;
     }
+    const colorName = NODE_COLORS[colorIndex % NODE_COLORS.length];
 
-    // colorIndex로 순환 처리
-    const index = colorIndex % NODE_COLORS.length;
-    return `bg-node-${NODE_COLORS[index]}-op-100`;
-};
+    return `var(--color-node-${colorName}-op-${opacity})`;
+}
