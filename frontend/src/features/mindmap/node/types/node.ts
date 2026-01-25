93a55987caa907853140df20de8f122879a917e0
EunseoPark <88071251+arty0928@users.noreply.github.com>
2026-01-25 21:46:53 +0900

    [FE] MenuNode 컴포넌트 구현 (#94)
    * feat: Node 컴포넌트에 NODE_COLORS 상수 배열 추가
    
    * feat: color 자체를 넘겨줄때, colorIndex를 넘겨줄 때 분기 처리, getColorClass 재사용
    
    * feat: MenuNodeButton 구현
    
    * feat: click 이벤트 리스너 생성
    
    * fix: tailwind v4에 맞춰 표준@layer로 변경
    
    * fix: getColorStyle bg외에 범용적으로 적용할 수 있도록 값만 반환  및 opacity 추가
    
    * feat: menuNodeComponent variant로 구현
    
    * chore: console.log 삭제
    
    * fix: ComponentPropsWithoutRef 로 변경(ref 없는 것으로)

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -1,4 +1,8 @@
+import { NODE_COLORS } from "@features/mindmap/node/utils/colors";
+
 export type NodeComponentProps = {
     colorIndex?: number;
+    color?: (typeof NODE_COLORS)[number];
+    opacity?: number;
     className?: string;
 };
--- frontend/src/features/mindmap/node/utils/colors.ts
+++ frontend/src/features/mindmap/node/utils/colors.ts
@@ -1,9 +1,16 @@
 import type { NodeComponentProps } from "@features/mindmap/node/types/node";
 
-const NODE_COLORS = ["violet", "blue", "skyblue", "mint", "cyan", "purple", "magenta", "navy"] as const;
+export const NODE_COLORS = ["violet", "blue", "skyblue", "mint", "cyan", "purple", "magenta", "navy"] as const;
 
-export const getColorClass = ({ colorIndex = 0 }: NodeComponentProps) => {
-    const index = colorIndex % NODE_COLORS.length;
+/*
+/**
+ * CSS 변수 형태로 색상 스타일 반환
+ */
+export function getColorStyle({ colorIndex, color, opacity }: NodeComponentProps): string {
+    if (color) {
+        return `var(--color-node-${color}-op-${opacity})`;
+    }
+    const colorName = NODE_COLORS[colorIndex % NODE_COLORS.length];
 
-    return `bg-node-${NODE_COLORS[index]}-op-100`;
-};
+    return `var(--color-node-${colorName}-op-${opacity})`;
+}
