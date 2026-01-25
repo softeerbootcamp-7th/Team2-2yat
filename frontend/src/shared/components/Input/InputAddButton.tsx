c9630970c1879669c0be926ce97f5325a213972f
arty0928 <arty0928@konkuk.ac.kr>
2026-01-25 21:27:47 +0900

    feat: InputAddButton 구현 및 클릭 이벤트 위임

---
+++ frontend/src/shared/components/Input/InputAddButton.tsx
@@ -0,0 +1,16 @@
+import Icon from "@shared/components/icon/Icon";
+import { ComponentPropsWithoutRef } from "react";
+
+type Props = ComponentPropsWithoutRef<"button"> & {};
+
+export default function InputAddButton({ ...rest }: Props) {
+    return (
+        <button
+            {...rest}
+            className="w-147.5 h-12 flex items-center justify-center gap-2 typo-body-14-medium cursor-pointer bg-base-white border-2 border-dashed border-gray-300 rounded-xl text-text-placeholder"
+        >
+            <Icon name="ic_plus" size={18} />
+            에피소드 추가
+        </button>
+    );
+}
