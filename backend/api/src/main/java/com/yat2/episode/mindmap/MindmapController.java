36253ff3573c0e1f596a42c0bbb9f628e38ee318
kimhji <twin4185@naver.com>
2026-01-27 03:10:00 +0900

    feat: 마인드맵 조회 인터페이스 생성

--- backend/api/src/main/java/com/yat2/episode/mindmap/MindmapController.java
+++ backend/api/src/main/java/com/yat2/episode/mindmap/MindmapController.java
@@ -1,4 +1,35 @@
 package com.yat2.episode.mindmap;
 
+import com.yat2.episode.mindmap.dto.MindmapDataDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class MindmapController  {
+    private final MindmapService mindmapService;
+
+    MindmapController(MindmapService mindmapService) {
+        this.mindmapService = mindmapService;
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<List<MindmapDataDto>> privateMindmap() {
+        //todo: userId 가져오기
+        //todo: 마인드맵_참여자 table에서 userId 기준 mindmap 데이터(shared = false) 가져오기
+        //todo: 즐겨찾기 기준으로 정렬하기
+
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/public")
+    public ResponseEntity<List<MindmapDataDto>> publicMindmap() {
+        //todo: userId 가져오기
+        //todo: 마인드맵_참여자 table에서 userId 기준 mindmap 데이터(shared = true) 가져오기
+        //todo: 즐겨찾기 기준으로 정렬하기
+
+        return ResponseEntity.ok(null);
+    }
 }
