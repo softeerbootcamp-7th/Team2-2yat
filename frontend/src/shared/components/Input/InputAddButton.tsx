f147063a724df15760c3fed8aeec8d181076cf75
arty0928 <arty0928@konkuk.ac.kr>
2026-01-26 13:49:10 +0900

    refactor: InputAddButton width를 full로

--- frontend/src/shared/components/Input/InputAddButton.tsx
+++ frontend/src/shared/components/Input/InputAddButton.tsx
@@ -7,7 +7,7 @@ export default function InputAddButton({ children, ...rest }: Props) {
     return (
         <button
             {...rest}
-            className="w-147.5 h-12 flex items-center justify-center gap-2 typo-body-14-medium cursor-pointer bg-base-white border-2 border-dashed border-gray-300 rounded-xl text-text-placeholder"
+            className="w-full h-12 flex items-center justify-center gap-2 typo-body-14-medium cursor-pointer bg-base-white border-2 border-dashed border-gray-300 rounded-xl text-text-placeholder"
         >
             <Icon name="ic_plus" size={18} />
             {children || "에피소드 추가"}
