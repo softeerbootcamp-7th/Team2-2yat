b3d2f738857c0c8889e092afc5aa5181914ee5e2
arty0928 <arty0928@konkuk.ac.kr>
2026-01-26 14:23:50 +0900

    refactor: Input,TextArea 파일 분리 및 input_set.ts 파일

--- frontend/src/shared/components/textarea/TextArea.tsx
+++ frontend/src/shared/components/textarea/TextArea.tsx
@@ -2,18 +2,16 @@ import { VariantProps } from "class-variance-authority";
 import { ComponentPropsWithoutRef } from "react";
 import { InputVariants } from "@shared/styles/input_variants";
 
-type SingleLineInputProps = ComponentPropsWithoutRef<"input"> & VariantProps<typeof InputVariants>;
+type MultilineInputProps = ComponentPropsWithoutRef<"textarea"> & VariantProps<typeof InputVariants>;
 
-const MAX_LENGTH = 43;
-
-function Input({ placeholder, value, inputSize = "full", ...rest }: SingleLineInputProps) {
+export function TextArea({ placeholder, value, inputSize = "full", ...rest }: MultilineInputProps) {
     const status = value ? "filled" : "empty";
+
     return (
-        <input
+        <textarea
             className={InputVariants({ inputSize, status })}
             placeholder={placeholder}
             value={value || ""}
-            maxLength={MAX_LENGTH}
             {...rest}
         />
     );
