94705924238e051ae72013454604017891f582b6
kimhji <twin4185@naver.com>
2026-01-26 18:50:52 +0900

    fix: Revert "fix: Merge remote-tracking branch 'origin/dev' into feat/#152/competency_type_domain"
    This reverts commit 43945d579de0ab5cae7904a1db33231c80d720f0, reversing
    changes made to 389a26ad1261e745debfb01cf1aa47b30a0902bb.
    
    # Conflicts:
    #	backend/api/build.gradle

--- backend/api/src/main/java/com/yat2/episode/users/UsersController.java
+++ backend/api/src/main/java/com/yat2/episode/users/UsersController.java
@@ -1,7 +1,5 @@
 package com.yat2.episode.users;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +15,6 @@ public class UsersController {
         this.usersService = usersService;
     }
 
-    @Operation(
-            summary = "summary 예시",
-            description = "사용자 리스트를 가져옵니다.",
-            security = {
-                    //@SecurityRequirement(name = "COOKIE_ACCESS_TOKEN")
-            }
-    )
     @GetMapping
     public ResponseEntity<List<Users>> getAllUsers() {
         return ResponseEntity.ok(usersService.getAllUsers());
