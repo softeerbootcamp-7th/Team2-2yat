6962c1fb24cbf42982b51d7a9c80ad2f4e8a885d
arty0928 <arty0928@konkuk.ac.kr>
2026-01-27 16:23:59 +0900

    feat: username 적용한 profile

---
+++ frontend/src/shared/components/profile/profile.tsx
@@ -0,0 +1,10 @@
+import Icon from "@shared/components/icon/Icon";
+
+export default function profile(name: string) {
+    return (
+        <div className="flex items-center gap-4">
+            <Icon name="ic_profile" color="var(--color-primary)" size={35} />
+            <span className="typo-body-16-medium pr-3">{name}님</span>
+        </div>
+    );
+}
