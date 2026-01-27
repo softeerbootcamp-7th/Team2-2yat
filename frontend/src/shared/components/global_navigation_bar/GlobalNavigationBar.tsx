49193bf2c3bd9018ae08ab601c30fb99c09f9da6
arty0928 <arty0928@konkuk.ac.kr>
2026-01-27 18:56:57 +0900

    feat: gnb contents toggle 방식으로 선택한 인덱스 전달

---
+++ frontend/src/shared/components/global_navigation_bar/GlobalNavigationBar.tsx
@@ -0,0 +1,52 @@
+import { ComponentPropsWithoutRef, useState } from "react";
+import Icon from "@shared/components/icon/Icon";
+import Button from "@shared/components/button/Button";
+import Row from "@shared/components/row/Row";
+import { cn } from "@utils/cn";
+
+type Props = ComponentPropsWithoutRef<"div"> & {
+    tabs: string[];
+    isLanding?: boolean;
+    onClick?: (index: number) => void;
+};
+
+export default function GlobalNavigationBar({ isLanding = false, tabs, onClick }: Props) {
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    return (
+        <div className={cn("w-full h-full p-4", isLanding ? "bg-base-white" : "bg-white-op-20")}>
+            <Row
+                className="h-full"
+                yPadding="none"
+                leftSlot={<Icon name="ic_logo" size="100%" color="var(--color-text-main1)" />}
+                contents={
+                    <div className="flex h-full gap-2">
+                        {tabs.map((tab, index) => (
+                            <Button
+                                key={index}
+                                variant={selectedTab === index ? "basic_accent" : "quaternary"}
+                                size="full"
+                                onClick={() => {
+                                    setSelectedTab(index);
+                                    onClick(index);
+                                }}
+                            >
+                                {tab}
+                            </Button>
+                        ))}
+                    </div>
+                }
+                rightSlot={
+                    <div className="flex h-full gap-2">
+                        <Button variant="quaternary_accent_outlined" size="full">
+                            로그인
+                        </Button>
+                        <Button variant="primary" size="full">
+                            회원가입
+                        </Button>
+                    </div>
+                }
+            />
+        </div>
+    );
+}
