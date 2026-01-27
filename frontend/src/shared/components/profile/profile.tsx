abdaa6cc059e934aedb6cd476c0f6e8f68110fd0
arty0928 <arty0928@konkuk.ac.kr>
2026-01-27 23:56:35 +0900

    feat: Profile componentProps 적용

--- frontend/src/shared/components/profile/profile.tsx
+++ frontend/src/shared/components/profile/profile.tsx
@@ -1,8 +1,13 @@
 import Icon from "@shared/components/icon/Icon";
+import { ComponentPropsWithoutRef } from "react";
 
-export default function Profile(name: string) {
+type Props = ComponentPropsWithoutRef<"div"> & {
+    name: string;
+};
+
+export default function Profile({ name, ...rest }: Props) {
     return (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4" {...rest}>
             <Icon name="ic_profile" color="var(--color-primary)" size={35} />
             <span className="typo-body-16-medium pr-3">{name}님</span>
         </div>
