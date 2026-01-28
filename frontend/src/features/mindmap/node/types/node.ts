a270144b80ee5313d288c9609b5d01998bf7dc24
arty0928 <arty0928@konkuk.ac.kr>
2026-01-29 00:38:23 +0900

    refactor: NodeComponentProps 삭제

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -1,9 +1,1 @@
-import { NodeColor } from "@features/mindmap/node/constants/colors";
-
 export type NodeState = "default" | "highlight" | "selected";
-
-export type NodeComponentProps = {
-    colorIndex?: number;
-    color?: NodeColor;
-    opacity?: number;
-};
