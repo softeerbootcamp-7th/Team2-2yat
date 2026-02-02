9b929bd7ffd7e01579989197cd56022624cd1468
arty0928 <arty0928@konkuk.ac.kr>
2026-02-02 09:19:54 +0900

    feat: 로그인 유무에 따라 gnb에 Userbox 보이기

--- frontend/src/features/home/pages/HomePage.tsx
+++ frontend/src/features/home/pages/HomePage.tsx
@@ -1,12 +1,29 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import Button from "@/shared/components/button/Button";
 import GlobalNavigationBar from "@/shared/components/global_navigation_bar/GlobalNavigationBar";
 import UserBox from "@/shared/components/user_box/UserBox";
+import { routeHelper } from "@/shared/utils/route";
 
 const HomePage = () => {
+    const { user } = useAuth();
+
     return (
         <>
-            <GlobalNavigationBar rightSlot={<UserBox name="asdf" />} />
+            <GlobalNavigationBar
+                rightSlot={
+                    user ? (
+                        <UserBox name={user.nickname} />
+                    ) : (
+                        <Link to={routeHelper.login()}>
+                            <Button size="xs" variant="quaternary_accent_outlined">
+                                로그인
+                            </Button>
+                        </Link>
+                    )
+                }
+            />
 
             <main>
                 <Outlet />
