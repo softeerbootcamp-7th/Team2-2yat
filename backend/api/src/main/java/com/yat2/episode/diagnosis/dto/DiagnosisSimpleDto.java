4ca07022e8e54d9ac3e0c9e2008cd1f3ff2aad65
kimhji <twin4185@naver.com>
2026-02-03 14:34:13 +0900

    refactor: 상수 처리 데이터 변수명 수정

--- backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleDto.java
+++ backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleDto.java
@@ -6,10 +6,10 @@ import com.yat2.episode.diagnosis.DiagnosisResult;
 
 public record DiagnosisSimpleDto(Integer diagnosisId, String diagnosisName, LocalDateTime createdAt,
                                  int lackCountOfCompetency) {
-    static final String diagnosisTemplate = " 직무 진단 결과";
+    private static final String DIAGNOSIS_TEMPLATE = " 직무 진단 결과";
 
-    static public DiagnosisSimpleDto of(DiagnosisResult diagnosisResult, int lackCountOfCompetency) {
-        return new DiagnosisSimpleDto(diagnosisResult.getId(), diagnosisResult.getJob().getName() + diagnosisTemplate,
+    public static DiagnosisSimpleDto of(DiagnosisResult diagnosisResult, int lackCountOfCompetency) {
+        return new DiagnosisSimpleDto(diagnosisResult.getId(), diagnosisResult.getJob().getName() + DIAGNOSIS_TEMPLATE,
                                       diagnosisResult.getCreatedAt(), lackCountOfCompetency);
     }
 }
--- backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleNoDateDto.java
+++ backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleNoDateDto.java
@@ -3,11 +3,11 @@ package com.yat2.episode.diagnosis.dto;
 import com.yat2.episode.diagnosis.DiagnosisResult;
 
 public record DiagnosisSimpleNoDateDto(Integer diagnosisId, String diagnosisName, int lackCountOfCompetency) {
-    static final String diagnosisTemplate = " 직무 진단 결과";
+    private static final String DIAGNOSIS_TEMPLATE = " 직무 진단 결과";
 
-    static public DiagnosisSimpleNoDateDto of(DiagnosisResult diagnosisResult, int lackCountOfCompetency) {
+    public static DiagnosisSimpleNoDateDto of(DiagnosisResult diagnosisResult, int lackCountOfCompetency) {
         return new DiagnosisSimpleNoDateDto(diagnosisResult.getId(),
-                                            diagnosisResult.getJob().getName() + diagnosisTemplate,
+                                            diagnosisResult.getJob().getName() + DIAGNOSIS_TEMPLATE,
                                             lackCountOfCompetency);
     }
 }
