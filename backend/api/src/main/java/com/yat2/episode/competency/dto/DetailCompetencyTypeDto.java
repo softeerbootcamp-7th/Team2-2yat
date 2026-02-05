757c7049f6dceeb73e45caacf98519d2e84d0748
w0uldy0u <woojoo@spacekim.org>
2026-02-05 13:46:23 +0900

    refactor: dto를 record로 변경

--- backend/api/src/main/java/com/yat2/episode/competency/dto/DetailCompetencyTypeDto.java
+++ backend/api/src/main/java/com/yat2/episode/competency/dto/DetailCompetencyTypeDto.java
@@ -1,19 +1,14 @@
 package com.yat2.episode.competency.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-
 import com.yat2.episode.competency.CompetencyType;
 
-@Getter
-@Builder
-public class DetailCompetencyTypeDto {
-    private int id;
-    private String competencyType;
-
-
+public record DetailCompetencyTypeDto(
+        int id,
+        CompetencyType.Category category,
+        String competencyType
+) {
     public static DetailCompetencyTypeDto of(CompetencyType competencyType) {
-        return DetailCompetencyTypeDto.builder().id(competencyType.getId()).competencyType(competencyType.getTypeName())
-                .build();
+        return new DetailCompetencyTypeDto(competencyType.getId(), competencyType.getCategory(),
+                                           competencyType.getTypeName());
     }
 }
